a4412b30ac7f098d5b44ce1518a553f736b9ad5d
Murat Aydogdu <murat.aydogdu@sap.com>
2020-06-24 17:21:48 +0300

    app-router and srv added works fine with trial xsuu on CF

---
+++ app/appdeployer/resources/browse/Component-dbg.js
@@ -0,0 +1,9 @@
+sap.ui.define(['sap/fe/core/AppComponent'], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend('c4u.flp.sample.books.Component', {
+        metadata: {
+            manifest: 'json'
+        }
+    });
+});
---
+++ app/browse/books/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("c4u.flp.sample.books.Component",{metadata:{manifest:"json"}})});
